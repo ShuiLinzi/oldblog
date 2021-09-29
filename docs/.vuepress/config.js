16c0c29ef3589bc84b9655cf2ecc3d2146be0f64
凛 <chenguoqinga@gmail.com>
2021-09-29 17:09:22 +0800

    :zap: 修改了多余js的加载

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,10 +9,7 @@ module.exports = {
   title: "凛",
   description: '决定未来的不是过去，而是现在。',
   head: [
-    ['link', {
-      rel: 'icon',
-      href: 'https://infinitypro-img.infinitynewtab.com/custom-icon/8001de1jd3n68lbfnxxt564xvb0vl5.png?imageMogr2/thumbnail/240x/format/webp/blur/1x0/quality/100|imageslim'
-    }],
+
     ['meta', {
       name: 'viewport',
       content: 'width=device-width,initial-scale=1,user-scalable=no'
@@ -21,11 +18,6 @@ module.exports = {
       "language": "javascript",
       "type": "text/javascript",
       "src": "https://cdn.staticfile.org/jquery/1.7.2/jquery.min.js"
-    }],
-    ["script", {
-      "language": "javascript",
-      "type": "text/javascript",
-      "src": "/js/MouseClickEffect.js"
     }]
   ],
 
